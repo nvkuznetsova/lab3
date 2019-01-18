7099d21afb0d22de00c08e2dbbec6300322e70e5
nvkuznetsova <nvkuznetsova@list.ru>
2019-01-18 14:19:43 +0300

    update

--- index.js
+++ index.js
@@ -81,7 +81,6 @@ app
     const URL = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20woeid%3D%222123260%22)%20and%20u%3D'c'&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
     const { data : { query : { results : { channel : { item : { forecast : [ , el1 ] }}}}}} = await get(URL, {'Content-Type' : 'application/json'});
     freqArr.push('weather');
-    //console.log(el1);
     res
       .set({'Content-Type' : 'text/html; charset=utf-8'})
       .send(`<h4>Date : ${el1.date}</h4>
