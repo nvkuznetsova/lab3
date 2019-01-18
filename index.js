63c6cbfed830004d255f359ad4ab40fe5b2cd044
nvkuznetsova <nvkuznetsova@list.ru>
2019-01-18 13:01:38 +0300

    add buffer

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const Parser = require('rss-parser');
 const parser = new Parser();
 const zlib = require('zlib');
 const trans = require('./trans');
+const http = require('http');
 
 app
   .use(bodyParser.json())
@@ -129,6 +130,44 @@ app
       .pipe(trans)
       .pipe(zlib.createGzip())
       .pipe(res);
+  })
+
+  //buffer
+  .get('/buffer', (req, res) => {
+    const URL = 'http://kodaktor.ru/api2/buffer2';
+    const num = 333;
+    const reg = /\w{4}/g;
+    let word = '';
+    const numsArr = [...Array.from(Array(2).keys(), x => x + num)];
+    const length = numsArr.length;
+    const resArr = [];
+    numsArr.forEach(number => {
+
+      http.get(`${URL}/${number}`, response => {
+        let data = '';
+        response.on('data', chunk => {
+          const found = String(chunk).match(reg);
+          if (found && !word) {
+            let [foundWord] = found;
+            word = foundWord;
+          }
+          data += chunk;
+        });
+        response.on('end', () => {
+          resArr.push({
+            number: number,
+            size: data.length,
+          });
+
+          if(resArr.length === length) {
+            let result = resArr.map((item) => {
+              return `Number: ${item.number}, Size (V2): ${item.size} <br>`;
+            })
+            res.send(`Secret word: <strong>${word}</strong> <br> ${result}`);
+          } 
+        });
+      });
+    });
   })
 
   //static
